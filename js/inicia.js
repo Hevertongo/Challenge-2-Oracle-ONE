9a4ea304673aef63ca6f28940beb91a1f4d45213
Heverton L <heverton.go@gmail.com>
2022-08-20 22:26:51 -0300

    adicionado script, corrigidos bugs visuais

--- js/inicia.js
+++ js/inicia.js
@@ -1,23 +1,75 @@
 
 // Variáveis
-var start = document.getElementById('start');
-var finalizaGame = document.getElementById('endGame')
+const botaoStart = document.getElementById('botaoStart');
+const botaoEndGame = document.getElementById('endGame');
+
+const gameLogo = document.getElementById('gameLogo');
+const listaForca = document.getElementsByClassName('itemForca');
+const teclado = document.getElementsByClassName('linhaTeclas');
+const palavraOculta = document.getElementById('palavraOculta');
+
+let respostasErradas = 0;
 
 
 // Chamadas de Funções
-	start.onclick = inicia;
-	finalizaGame.onclick = finaliza;
 
+	// escondeTodos(listaForca);
+	esconde (palavraOculta);
+	botaoStart.onclick = inicia;
+	botaoEndGame.onclick = finaliza;
 
 // Funções
-	function finaliza() {
-		// body...
-		document.getElementById('gameLogo').style.display = '';
-		document.getElementById('start').style.display = '';
-	}
+function inicia() {
+	esconde (gameLogo);
+	esconde (botaoStart);
+	desenhaForca();
+	exibe (palavraOculta);
+}
 
-	function inicia() {
-		// body...
-		document.getElementById('gameLogo').style.display = 'none';
-		document.getElementById('start').style.display = 'none';
+function finaliza() {
+	exibe (gameLogo);
+	exibe (botaoStart);
+	escondeTodos(listaForca);
+	esconde (palavraOculta);
+}
+
+function escondeTodos(lista) {
+	for (const item of lista) {
+	    esconde (item);
 	}
+}
+
+function desenhaForca() {
+  switch (respostasErradas) {
+    case 0:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 1:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 2:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 3:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 4:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 5:
+		exibe (listaForca[respostasErradas]);
+		break
+    case 6:
+		exibe (listaForca[respostasErradas]);
+		break
+    default:
+  }
+}
+
+function esconde(elemento) {
+    elemento.style.display = 'none';
+}
+
+function exibe(elemento) {
+    elemento.style.display = '';
+}

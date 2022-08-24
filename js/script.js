56b139df90aefa1422c48d09fe7838309a175c02
Heverton L <heverton.go@gmail.com>
2022-08-24 20:45:36 -0300

    Criada estrutura de verificação de vitória ou derrota

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // Variáveis
 const listaForca = document.getElementsByClassName('itemForca');
 const listaInicial = document.getElementsByClassName('inicial');
@@ -8,17 +7,26 @@ const botaoRecomecar = document.getElementById('recomecar');
 const botaoPulaEssa = document.getElementById('pula');
 const botaoVerPalavras = document.getElementById('verPalavras');
 const botaoNovaPalavra = document.getElementById('novaPalavra');
+const avisos = document.getElementById('avisos');
 
 const teclado = document.getElementsByClassName('linhaTeclas');
 const palavraOculta = document.querySelector('#palavraOculta');
+const dicaElemento = document.querySelector('#dica');
 
-let respostasErradas = 0, vitorias = 0, derrotas =0; respostaOculta = []; resposta = '';
-let acertouPalavra = true; jogoRodando = false;
-let palavras = [
+let letrasErradas = 0, vitorias = 0, derrotas =0; respostaOculta = []; resposta = '';
+let acertouPalavra = true;
+var palavras = [
 'camelo',
 'mesa',
 'geladeira',
 'vaso'
+];
+
+var dicas = [
+'animal',
+'objeto',
+'cozinha',
+'planta'
 ];
 
 // Chamadas de Funções
@@ -30,25 +38,101 @@ let palavras = [
 
 // Funções
 function inicia() {
-	respostasErradas = 0;
-	resposta = sorteiaPalavra().toUpperCase();
-		console.log(resposta);
+	letrasErradas = 0; vitorias = 0; derrotas =0; acertouPalavra = true;
 	ocultaTodos(listaInicial);
-	exibeTodos(listaInGame);
-	respostaOculta = [];
-	imprimePalavraOculta();
-	desenhaForca();
-	preencheTeclado();
+	proximaPalavraOculta();
 }
 
 function finaliza() {
 	ocultaTodos(listaForca);
 	ocultaTodos(listaInGame);
 	exibeTodos(listaInicial);
-	limpaTeclado();
+	reiniciaTeclado();
 	limpaAcertos();
 	palavraOculta.textContent = '';
-	respostasErradas = 0; vitorias = 0; derrotas =0; acertouPalavra = true; jogoRodando = false;
+	dicaElemento.textContent = '';
+	avisos.textContent = '';
+}
+
+function proximaPalavraOculta () {
+	palavraOculta.textContent = '';
+	dicaElemento.textContent = '';
+	avisos.textContent = "";
+	letrasErradas = 0;
+	respostaOculta = [];
+	reiniciaTeclado();
+	novaResposta();
+	imprimePalavraOculta();
+	desenhaForca();
+	preencheTeclado();
+	exibeTodos(listaInGame);
+}
+
+function confereLetra() {
+	let letraChave = this.innerText.toUpperCase();
+  this.classList.toggle('letraClicada', true)
+  this.removeEventListener('click', confereLetra)
+	if (resposta.includes(letraChave)){
+		// Se acerta a letra
+		for (var i in respostaOculta) {
+			if (resposta[i] == letraChave) respostaOculta[i] = resposta[i];
+		}
+		atualizarExibirPalavra();
+		if (respostaOculta.includes('_') == false) {
+			// Se acabam os _
+			acertouPalavra = true;
+			palavraCertaOuErrada();
+			// Verificar fim da palavra ou do jogo
+			fimDaPalavra();
+			// proximaPalavraOculta();
+		}
+  } else {
+    // Quando erra a letra
+    this.classList.toggle('letraClicada', true)
+    this.removeEventListener('click', confereLetra)
+    letrasErradas++;
+    desenhaForca();
+  }
+}
+
+function fimDaPalavra() {
+		ocultaTodos(listaInGame);
+		ocultaTodos(listaForca);
+		reiniciaTeclado();
+	if (acertouPalavra) {
+		avisos.textContent = 'Palavra correta!';
+	} else {
+		palavraOculta.textContent = resposta;
+		avisos.textContent = 'Palavra incorreta!';
+	}
+	vitoriaOuDerrota();
+}
+
+function vitoriaOuDerrota() {
+	if (vitorias == 4 || derrotas == 4) {
+		if (vitorias > derrotas) {
+			avisos.textContent = 'Parabéns! Você Venceu!';
+			avisos.onclick = finaliza;
+		} else {
+			avisos.textContent = 'Você perdeu!';
+			avisos.onclick = finaliza;
+		}
+	} else {
+		avisos.onclick = proximaPalavraOculta;
+	}
+}
+
+function pulaEssa() {
+	acertouPalavra = false;
+	palavraCertaOuErrada();
+	fimDaPalavra();
+}
+
+function novaResposta() {
+	index = sorteiaIndex();
+	resposta = palavras[index].toUpperCase();
+	dica = dicas[index].toUpperCase();
+	dicaElemento.textContent = dica;
 }
 
 function ocultaTodos(lista) {
@@ -82,33 +166,14 @@ function preencheLinha (linha, letras) {
 		let tecla = document.createElement('a')
  		tecla.id = l.toUpperCase()
  		tecla.append(l)
- 		tecla.addEventListener('click', confereChave)
+ 		tecla.addEventListener('click', confereLetra)
  		document.querySelector('#tecladoLinha_' + linha).append(tecla)
  	}
 }
 
-function limpaTeclado() {
+function reiniciaTeclado() {
   for (var i of document.querySelectorAll('.areaTeclado div')) //limpa o teclado
     i.innerText = ''
-}
-
-
-function confereChave() {
-	let letraChave = this.innerText.toUpperCase();
-	if (resposta.includes(letraChave)){
-		for (var i in respostaOculta) {
-			if (resposta[i] == letraChave) respostaOculta[i] = resposta[i];
-		}
-		atualizarExibirPalavra();
-    this.classList.toggle('letraCorreta', true)
-    this.removeEventListener('click', confereChave)
-  } else {
-    // Quando erra a letra
-    this.classList.toggle('letraIncorreta', true)
-    this.removeEventListener('click', confereChave)
-    respostasErradas++;
-    desenhaForca();
-  }
 }
 
 function imprimePalavraOculta () {
@@ -124,26 +189,22 @@ function atualizarExibirPalavra() {
   palavraOculta.textContent = display
 }
 
-function sorteiaPalavra() {
-  return palavras[Math.floor(Math.random() * palavras.length)]
+function sorteiaIndex() {
+  return Math.floor(Math.random() * palavras.length);
 }
 
 function palavraCertaOuErrada () {
 	if (acertouPalavra) {
+		vitorias++;
 		let img = document.createElement('img');
 		img.src = 'img/acerto.png';
 		document.querySelector('#totalAcertos').append(img);
 	} else {
+		derrotas++;
 		let img = document.createElement('img');
 		img.src = 'img/erro.png';
 		document.querySelector('#totalAcertos').append(img);
 	}
-}
-
-function pulaEssa() {
-	acertouPalavra = false;
-	derrotas++;
-	palavraCertaOuErrada();
 }
 
 function limpaAcertos() {
@@ -153,27 +214,31 @@ function limpaAcertos() {
 }
 
 function desenhaForca() {
-  switch (respostasErradas) {
+  switch (letrasErradas) {
     case 0:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 1:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 2:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 3:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 4:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 5:
-		exibe (listaForca[respostasErradas]);
-		break
+			exibe (listaForca[letrasErradas]);
+			break
     case 6:
-		exibe (listaForca[respostasErradas]);
+			exibe (listaForca[letrasErradas]);
+			acertouPalavra = false;
+			palavraCertaOuErrada();
+			// Verificar fim da palavra ou do jogo
+			fimDaPalavra();
 		break
     default:
   }
